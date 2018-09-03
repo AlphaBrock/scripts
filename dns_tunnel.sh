acfbc9a795b2ffcf977f8d4472309f79d446d2b5
AlphaBrock <linjcciam1314@yahoo.com>
2018-09-03 14:44:25 +0800

    fix error

--- dns_tunnel.sh
+++ dns_tunnel.sh
@@ -127,14 +127,6 @@ get_char(){
     stty $SAVEDSTTY
 }
 
-#get server ip
-get_ip(){
-    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
-    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
-    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
-    echo ${IP}
-}
-
 # Pre-installation settings
 pre_install(){
     if check_sys packageManager yum || check_sys packageManager apt; then
