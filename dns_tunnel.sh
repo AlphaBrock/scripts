9438ada38d32206806e8d9069c7ebb97b040361f
AlphaBrock <linjcciam1314@yahoo.com>
2018-10-30 17:28:05 +0800

    Update dns_tunnel.sh

--- dns_tunnel.sh
+++ dns_tunnel.sh
@@ -441,22 +441,21 @@ com_softether(){
 config_system_start(){
     mv /etc/rc.local /etc/rc.local.bk 
     cat > /etc/rc.local<<-EOF
-    #!/bin/sh -e
-    #
-    # rc.local
-    #
-    # This script is executed at the end of each multiuser runlevel.
-    # Make sure that the script will "exit 0" on success or any other
-    # value on error.
-    #
-    # In order to enable or disable this script just change the execution
-    # bits.
-    #
-    # By default this script does nothing.
-    /root/vpnserver/vpnserver start
-    exit 0
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+/root/vpnserver/vpnserver start
+exit 0
 EOF
-
     chmod +x /etc/rc.local
 }
 
